4eade02125ba5c3f61972c6039fb26fcbd9dd37d
AleksandrYashchenko <aleksandr.yashchenko1@gmail.com>
2021-04-27 17:55:28 +0300

    Adding test for search, refactoring.

--- TestGoogleNUnit/TestGoogleNUnit/UnitTest1.cs
+++ TestGoogleNUnit/TestGoogleNUnit/UnitTest1.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 
@@ -42,15 +43,15 @@ namespace TestGoogleNUnit
         [Test]
         public void Registration()
         {
-            var registrationButton = driver
+            IWebElement registrationButton = driver
                 .FindElement(By.XPath("//div[@class='ek-grid__item']/a[text()='зарегистрироваться']"));
             registrationButton.Click();
 
-            var nameRegistration = driver                
+            IWebElement nameRegistration = driver
                 .FindElement(By.XPath("//div[@class='lp-vertical-form__field']/input[@data-qaid='name']"));
-            var emailRegistration = driver
+            IWebElement emailRegistration = driver
                 .FindElement(By.XPath("//div[@class='lp-vertical-form__field']/input[@data-qaid='email']"));
-            var passwordRegistration = driver
+            IWebElement passwordRegistration = driver
                 .FindElement(By.XPath("//div[@class='lp-vertical-form__field']/input[@data-qaid='password']"));
 
             string name = RandomString();
@@ -62,11 +63,11 @@ namespace TestGoogleNUnit
             string password = RandomString();
             passwordRegistration.SendKeys(password);
 
-            var buttonLogin = driver
+            IWebElement buttonLogin = driver
                 .FindElement(By.XPath("//button[@data-qaid='submit']"));
             buttonLogin.Click();
 
-            var transitionEmail = driver
+            IWebElement transitionEmail = driver
                 .FindElement(By.XPath("//div[@class='b-swipe-tabs']/a[@data-qaid='credentials_settings_tab']"));
             transitionEmail.Click();
 
@@ -78,9 +79,37 @@ namespace TestGoogleNUnit
             Assert.AreEqual(email+"@gmail.com", checkedEmail, "Email doesn't match the one entered during registration");
         }
 
-        public class SearchTest
+        public class SearchTest : BaseTest
         {
+            [Test]
+            public void Search()
+            {
+                IWebElement searchLine = driver
+                    .FindElement(By.XPath("//input[@name='search_term']"));
+                searchLine.Click();
 
+                string searchKey = "Велосипед";
+                searchLine.SendKeys(searchKey);
+
+                IWebElement searchButton = driver
+                    .FindElement(By.XPath("//button[@type='submit']"));
+                searchButton.Click();
+
+                // TODO: В дальнейшем нужно исправить Thread.Sleep на Wait.
+                Thread.Sleep(10000); 
+                IReadOnlyCollection<IWebElement> productsGallary = driver.
+                    FindElements(By.XPath(".//span[contains(@class, 'ek-text_wrap_two-line')]"));
+
+                foreach (IWebElement product in productsGallary)
+                {
+                    // Переменная сохраняющая значение которое храниться в title.
+                    string titleValue = product.Text;
+
+                    // Проверка, присутсвует ли слово велосипед в элементе коллекции.
+                    bool isSearchKeyPresentInTitle = titleValue.Contains(searchKey, StringComparison.InvariantCultureIgnoreCase);
+
+                    Assert.IsTrue(isSearchKeyPresentInTitle, "Test failed, search is not working correctly");
+                }
             }
         }
     }
